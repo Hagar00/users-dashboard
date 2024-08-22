d8c22817e700c35c77a943c7e8c550f9aeb7efef
Hagar Hassan <hagar.hassan@nweave.com>
2024-08-23 01:04:05 +0300

    create styles of user dashboard and card of details

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,15 +7,24 @@ import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { User } from '../models/user';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule,MatPaginatorModule, MatCardModule,CommonModule,HttpClientModule],
+  imports: [FormsModule,MatPaginatorModule,
+     MatCardModule,CommonModule,
+     HttpClientModule,FontAwesomeModule],
   templateUrl: './home.component.html',
+  template: `<fa-icon [icon]="faUser"></fa-icon>`,
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  faUser = fas['faUser'];
 @Input() users:User[] = [];
   totalUsers = 0;
 
