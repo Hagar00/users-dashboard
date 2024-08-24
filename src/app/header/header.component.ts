aa841f1a2d5f2b43357432c50f02475f5007a6cc
Hagar Hassan <hagar.hassan@nweave.com>
2024-08-24 15:39:15 +0300

    create navbar has logo and icons of my linkes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  faGitHub = faGithub;
+  faLinkedIn = faLinkedin;
+
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faGithub, faLinkedin);
+  }
 }
